08fa50f45e169e33c20153f2200c7e48c3a2b2d0
sszz01 <resios027@gmail.com>
2024-10-20 22:31:51 -0700

    Initial Commit

---
+++ ParkLens-AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkLens-AI
+//
+//  Created by Oleksandr Voievodin on 10/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
