283a27c04616fff8f83ed05069e191fca6d9b909
maxkex <mkebets@gmail.com>
2024-10-24 09:33:54 -0700

    Second Commit

---
+++ ParkLens-AI/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Parking Lot Detector AI Prototype
+//
+//  Created by Oleksandr Voievodin on 7/22/24.
+//
+
+import SwiftUI
+import PhotosUI
+import AVKit
+
+struct ContentView: View {
+    @State private var isPickerPresented = false
+    @State private var selectedImage: UIImage?
+    @State private var selectedVideoURL: URL?
+    
+    var body: some View {
+        VStack {
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            } else if let videoURL = selectedVideoURL {
+                VideoPlayer(player: AVPlayer(url: videoURL))
+                    .frame(width: 300, height: 300)
+            }
+            
+            Button(action: {
+                isPickerPresented = true
+            }) {
+                Text("Select Photo/Video")
+            }
+            .padding()
+            .sheet(isPresented: $isPickerPresented) {
+                MediaPicker(isPresented: $isPickerPresented, selectedImage: $selectedImage, selectedVideoURL: $selectedVideoURL)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkLens-AI/ParkLens_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkLens_AIApp.swift
+//  ParkLens-AI
+//
+//  Created by Oleksandr Voievodin on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkLens_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
