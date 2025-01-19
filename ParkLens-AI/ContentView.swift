788bbe82edf3ed3bf59d72832ebfb7e855f5a20e
sszz01 <resios027@gmail.com>
2025-01-18 18:25:07 -0800

    actually deleted everything now

--- ParkLens-AI/ContentView.swift
+++ ParkLens-AI/ContentView.swift
@@ -6,35 +6,12 @@
 //
 
 import SwiftUI
-import PhotosUI
-import AVKit
 
 struct ContentView: View {
-    @State private var isPickerPresented = false
-    @State private var selectedImage: UIImage?
-    @State private var selectedVideoURL: URL?
     
     var body: some View {
         VStack {
-            if let image = selectedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 300)
-            } else if let videoURL = selectedVideoURL {
-                VideoPlayer(player: AVPlayer(url: videoURL))
-                    .frame(width: 300, height: 300)
-            }
-            
-            Button(action: {
-                isPickerPresented = true
-            }) {
-                Text("Select Photo/Video")
-            }
-            .padding()
-            .sheet(isPresented: $isPickerPresented) {
-                MediaPicker(isPresented: $isPickerPresented, selectedImage: $selectedImage, selectedVideoURL: $selectedVideoURL)
-            }
+            Text("Hello, World!")
         }
     }
 }
