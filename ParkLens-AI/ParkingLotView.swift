fa4f28ad90453bfa97010f58d8cb89dcca2a8b16
sszz01 <resios027@gmail.com>
2025-02-23 14:38:12 -0800

    App reworked to show 4 buttons, each has a stream url that is displayed using AVPlayer.

---
+++ ParkLens-AI/ParkingLotView.swift
@@ -0,0 +1,18 @@
+//
+//  ParkingLotView.swift
+//  ParkLens-AI
+//
+//  Created by Oleksandr Voievodin on 1/23/25.
+//
+
+import SwiftUI
+
+struct ParkingLotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkingLotView()
+}
