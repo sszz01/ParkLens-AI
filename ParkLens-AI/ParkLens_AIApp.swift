5b71456bf0197df98781632229328c7a19f8cae0
sszz01 <resios027@gmail.com>
2025-02-23 14:41:13 -0800

    follow up with previous commit

--- ParkLens-AI/ParkLens_AIApp.swift
+++ ParkLens-AI/ParkLens_AIApp.swift
@@ -12,6 +12,7 @@ struct ParkLens_AIApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.light)
         }
     }
 }
--- ParkLens-AI/ParkingLotView.swift
+++ ParkLens-AI/ParkingLotView.swift
@@ -5,14 +5,70 @@
 //  Created by Oleksandr Voievodin on 1/23/25.
 //
 
+import AVKit
 import SwiftUI
 
 struct ParkingLotView: View {
+    let lotNumber: Int
+    @State private var player: AVPlayer?
+    @Environment(\.dismiss) private var dismiss
+    
+    var streamURL: URL {
+        switch lotNumber {
+        case 1:
+            return URL(string: "http://192.168.4.171:8080/hls/stream.m3u8")!
+        case 2:
+            return URL(string: "http://192.168.1.8:8080/hls/stream2.m3u8")!
+        case 3:
+            return URL(string: "http://192.168.1.8:8080/hls/stream3.m3u8")!
+        case 4:
+            return URL(string: "http://192.168.1.8:8080/hls/stream4.m3u8")!
+        default:
+            return URL(string: "http://192.168.1.8:8080/hls/stream1.m3u8")!
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                if let player = player {
+                    VideoPlayer(player: player)
+                        .onAppear {
+                            player.play()
+                        }
+                        .onDisappear {
+                            player.pause()
+                        }
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(maxWidth: .infinity, maxHeight: 300)
+                } else {
+                    Text("Loading video...")
+                }
+            }
+            .navigationTitle(Text("Parking Lot \(lotNumber)"))
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "arrow.left.circle")
+                            Text("Back")
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                player = AVPlayer(url: streamURL)
+            }
+        }
     }
 }
 
+
 #Preview {
-    ParkingLotView()
+    ParkingLotView(lotNumber: 1)
 }
